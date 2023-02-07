bed995547e1d5bbd80ca1e7813d8b37e3d0ab8df
kavyakunder <kavya.k@deuexsolutions.com>
2023-02-07 16:49:33 +0530

    feat : loading, empty-input, invalid-user

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import "./App.css";
 import { useState } from "react";
 
-function App() {
-  const githubapi = "https://api.github.com/users/";
+const GITHUB_API = "https://api.github.com/users/";
 
+function App() {
   const [username, setUserName] = useState("");
-  const [userData, setUserData] = useState({});
-  const [displayMsg, setDisplayMsg] = useState(true);
+  const [userData, setUserData] = useState("");
+  const [err, setErr] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [emptyInput, setEmptyInput] = useState(false);
 
   const fetchUserName = async () => {
-    const response = await fetch(githubapi + username);
-    const data = await response.json();
-    setUserData(data);
-    setDisplayMsg(false);
+    setLoading(true);
+    setErr(false);
+    if (username === "") {
+      setEmptyInput(true);
+      setLoading(false);
+    } else {
+      const response = await fetch(GITHUB_API + username);
+      if (response.status === 200) {
+        const data = await response.json();
+        setErr(false);
+        setLoading(false);
+        setUserData(data);
+        setEmptyInput(false);
+      } else {
+        setErr(true);
+        setEmptyInput(false);
+        setLoading(false);
+      }
+    }
   };
 
   return (
@@ -24,25 +41,37 @@ function App() {
       <button className="btn-fetch" onClick={fetchUserName}>
         Fetch User
       </button>
-      {!displayMsg ? (
-        <div className="user-info">
-          <div className="personal-info">
-            <h1>Personal Info</h1>
-            <h2>Name: {userData.name}</h2>
-            <h2>Bio: {userData.bio}</h2>
-            <h2>Username: @{userData.login}</h2>
-            <h2>Company: {userData.company}</h2>
-            <h2>Location: {userData.location}</h2>
-          </div>
-          <div className="github-info">
-            <h1>Github Info</h1>
-            <h2>Public Repos : {userData.public_repos}</h2>
-            <h2>Followers: {userData.followers}</h2>
-            <h2>Following: {userData.following}</h2>
-          </div>
-        </div>
+      {emptyInput ? (
+        <h1>Username cannot be empty</h1>
       ) : (
-        <h1>Search for user</h1>
+        <>
+          {!err ? (
+            <>
+              {!loading ? (
+                <div className="user-info">
+                  <div className="personal-info">
+                    <h1>Personal Info</h1>
+                    <h2>Name: {userData.name}</h2>
+                    <h2>Bio: {userData.bio}</h2>
+                    <h2>Username: @{userData.login}</h2>
+                    <h2>Company: {userData.company}</h2>
+                    <h2>Location: {userData.location}</h2>
+                  </div>
+                  <div className="github-info">
+                    <h1>Github Info</h1>
+                    <h2>Public Repos : {userData.public_repos}</h2>
+                    <h2>Followers: {userData.followers}</h2>
+                    <h2>Following: {userData.following}</h2>
+                  </div>
+                </div>
+              ) : (
+                <h1>Loading</h1>
+              )}
+            </>
+          ) : (
+            <h1>User not found</h1>
+          )}
+        </>
       )}
     </div>
   );
