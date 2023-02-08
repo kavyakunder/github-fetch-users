93a7488bac6a7a8a6a78dfc3d56224343b302cb8
kavyakunder <kavya.k@deuexsolutions.com>
2023-02-08 15:25:48 +0530

    refactor : data map

--- src/components/GithubInfo.jsx
+++ src/components/GithubInfo.jsx
@@ -1,11 +1,23 @@
 const GithubInfo = ({ userData }) => {
-  const { public_repos, followers, following } = userData;
+  const { public_repos, public_gists, followers, following, type } = userData;
+  const githubInfo = {
+    "Public Repos": public_repos,
+    "Public Gists": public_gists,
+    Followers: followers,
+    Following: following,
+    Type: type,
+  };
+
   return (
     <div className="github-info">
       <h1>Github Info</h1>
-      <h2>Public Repos : {public_repos}</h2>
-      <h2>Followers: {followers}</h2>
-      <h2>Following: {following}</h2>
+      {Object.entries(githubInfo).map(([key, val]) => {
+        return (
+          <p key={key}>
+            {key} : {val}
+          </p>
+        );
+      })}
     </div>
   );
 };
--- src/components/PersonInfo.jsx
+++ src/components/PersonInfo.jsx
@@ -1,13 +1,23 @@
 const PersonInfo = ({ userData }) => {
   const { name, bio, login, company, location } = userData;
+  const personInfo = {
+    Name: name,
+    Bio: bio,
+    Login: login,
+    Company: company,
+    Location: location,
+  };
+
   return (
     <div className="personal-info">
       <h1>Personal Info</h1>
-      <h2>Name: {name}</h2>
-      <h2>Bio: {bio}</h2>
-      <h2>Username: @{login}</h2>
-      <h2>Company: {company}</h2>
-      <h2>Location: {location}</h2>
+      {Object.entries(personInfo).map(([key, val]) => {
+        return (
+          <p key={key}>
+            {key} : {val}
+          </p>
+        );
+      })}
     </div>
   );
 };
